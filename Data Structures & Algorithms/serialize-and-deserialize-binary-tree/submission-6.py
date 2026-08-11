4a8357982ff84a718f924bc9ae7ea21c2245a3f1
c9zA <177842201@qq.com>
2026-08-10 20:02:05 -0400

    Add: serialize-and-deserialize-binary-tree - submission-6

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-6.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Codec:
+    
+    # Encodes a tree to a single string.
+    def dfsSerialize(self, arr, root):
+        arr.append(str(root.val))
+        if root.left:
+            self.dfsSerialize(arr, root.left)
+        else:
+            arr.append('n')
+        if root.right:
+            self.dfsSerialize(arr, root.right)
+        else:
+            arr.append('n')
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        ans = []
+        if not root:
+            return ""
+        self.dfsSerialize(ans, root)
+        return ','.join(ans)
+
+        
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        if not data:
+            return None
+        arr = data.split(",")
+        idx = 0
+        def dfs(arr):
+            nonlocal idx
+            r = TreeNode(int(arr[idx]))
+            idx += 1
+            if idx<len(arr) and arr[idx]!='n':
+                r.left = dfs(arr)
+            idx += 1
+            if idx<len(arr) and arr[idx]!='n':
+                r.right = dfs(arr)
+            return r
+        return dfs(arr)
