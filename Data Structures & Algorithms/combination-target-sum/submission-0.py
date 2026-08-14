676f3627ac8c8b2becff5622af71c660e862e66f
c9zA <177842201@qq.com>
2026-08-14 00:19:16 -0400

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        hset = set(nums)
+        ans = []
+        res = []
+        def recurse(target, mx):
+            nonlocal res
+            if not hset:
+                return
+            if target<2:
+                if target==0:
+                    ans.append(res.copy())
+                return
+            arr = list(hset)
+            for ele in arr:
+                if ele<mx:
+                    continue
+                res.append(ele)
+                recurse(target-ele, max(mx, ele))
+                res.pop()
+        recurse(target, 0)
+        return ans
+
